b366216438fe5240fcdf906f559959d8d8ddfad2
Alice <ciceribeiroo@gmail.com>
2020-04-09 16:14:35 -0300

    Adicionando testes

---
+++ APIComprasTeste/APICompras/Services/ICompraService.cs
@@ -0,0 +1,13 @@
+using APICompras.Models;
+using System;
+using System.Collections.Generic;
+namespace APICompras.Services
+{
+    public interface ICompraService
+    {
+        IEnumerable<CompraItem> GetAllItems();
+        CompraItem Add(CompraItem novoItem);
+        CompraItem GetById(int id);
+        void Remove(int id);
+    }
+}
